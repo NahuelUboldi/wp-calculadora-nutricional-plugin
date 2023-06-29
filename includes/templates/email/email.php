549fca508e89377d49facd53bb4e773cade15751
NahuelUboldi <nahueluboldi@gmail.com>
2023-06-29 17:44:42 -0300

    working on the email template

---
+++ includes/templates/email/email.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>Your Message Subject or Title</title>
+	<style type="text/css">
+		#outlook a {padding:0;}
+		body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
+		.ExternalClass {width:100%;}
+		.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;}
+		#backgroundTable {margin:0; padding:0; width:100% !important; line-height: 100% !important;}
+		img {outline:none; text-decoration:none; -ms-interpolation-mode: bicubic;}
+		a img {border:none;}
+		.image_fix {display:block;}
+		p {margin: 0;padding:5px;}
+		h1, h2, h3, h4, h5, h6 {color: black !important;}
+		h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {color: blue !important;}
+		h1 a:active, h2 a:active,  h3 a:active, h4 a:active, h5 a:active, h6 a:active {
+			color: red !important;
+		 }
+		h1 a:visited, h2 a:visited,  h3 a:visited, h4 a:visited, h5 a:visited, h6 a:visited {
+			color: purple !important;
+		}
+		table td {border-collapse: collapse;}
+		table { border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
+		a {color: orange;}
+		@media only screen and (max-device-width: 480px) {
+			a[href^="tel"], a[href^="sms"] {
+						text-decoration: none;
+						color: black; /* or whatever your want */
+						pointer-events: none;
+						cursor: default;
+					}
+			.mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+						text-decoration: default;
+						color: orange !important; /* or whatever your want */
+						pointer-events: auto;
+						cursor: default;
+					}
+
+					
+		}
+
+		@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+			a[href^="tel"], a[href^="sms"] {
+						text-decoration: none;
+						color: blue;
+						pointer-events: none;
+						cursor: default;
+					}
+			.mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+						text-decoration: default;
+						color: orange !important;
+						pointer-events: auto;
+						cursor: default;
+					}
+		}
+		@media only screen and (-webkit-min-device-pixel-ratio: 2) {
+			/* Put your iPhone 4g styles in here */
+		}
+		@media only screen and (-webkit-device-pixel-ratio:.75){
+			/* Put CSS for low density (ldpi) Android layouts in here */
+		}
+		@media only screen and (-webkit-device-pixel-ratio:1){
+			/* Put CSS for medium density (mdpi) Android layouts in here */
+		}
+		@media only screen and (-webkit-device-pixel-ratio:1.5){
+			/* Put CSS for high density (hdpi) Android layouts in here */
+		}
+				@media screen and (max-width:600px) {
+				.block { display: block !important; width: 100% !important; }
+		}
+	</style>
+	<!--[if IEMobile 7]>
+	<style type="text/css">
+		/* Targeting Windows Mobile */
+	</style>
+	<![endif]-->
+	<!--[if gte mso 9]>
+	<style>
+		/* Target Outlook 2007 and 2010 */
+	</style>
+	<![endif]-->
+</head>
+<body>
+	<table cellpadding="0" cellspacing="0" border="0" id="backgroundTable" style="background:#eee;">
+	<tr>
+		<td>
+
+		<!--table start -->
+			<table cellpadding="0" cellspacing="0" border="0" align="center" role="presentation" style="max-width:600px;border:solid 1px black;background:white; margin:5px auto">
+							<tr style="background:#FED1EE;">
+										<td class="block" align="left" valign="center" style="padding:1%; " width="20%">
+													
+											<!--inner table start -->
+													<table cellpadding="0" cellspacing="0" border="1" align="center" role="presentation" style="width:97%;margin-bottom: 10px">
+																	<tr style="border-bottom:solid 1px black;background:#FF8AD8;">
+																				<td align="center" valign="center" >
+																					<p>
+																						Peso
+																					</p>
+																				</td>
+																	</tr>
+																	<tr style="background:#fff;">
+																				<td align="center" valign="center" >
+																						<p>
+																								66,1
+																						</p>
+																				</td>
+																	</tr>
+													</table>
+													<!--inner table end -->
+
+													<!--inner table start -->
+													<table cellpadding="0" cellspacing="0" border="1" align="center" role="presentation" style="width:97%;margin-bottom: 10px">
+																	<tr style="border-bottom:solid 1px black;background:#FF8AD8;">
+																				<td align="center" valign="center" >
+																					<p>
+																						IMC
+																					</p>
+																				</td>
+																	</tr>
+																	<tr style="background:#fff;">
+																				<td align="center" valign="center" >
+																						<p>
+																								66,1
+																						</p>
+																				</td>
+																	</tr>
+													</table>
+													<!--inner table end -->
+
+												</td>
+												<td class="block" align="left" valign="center" style="padding:1%;" width="80%">
+																	<img src="http://team-hh.com/wp-content/uploads/2023/06/image-3.webp" width="98%" />
+										</td>
+							</tr>
+			</table>
+			<!--table end -->
+
+
+
+		<!--table start -->
+			<table cellpadding="0" cellspacing="0" border="0" align="center" role="presentation" style="max-width:600px;border:solid 1px black;background:white; margin:5px auto">
+							<tr style="background:#FED1EE;">
+										<td class="block" align="left" valign="center" style="padding:1%; " width="20%">
+													
+											<!--inner table start -->
+													<table cellpadding="0" cellspacing="0" border="1" align="center" role="presentation" style="width:97%;margin-bottom: 10px">
+																	<tr style="border-bottom:solid 1px black;background:#FF8AD8;">
+																				<td align="center" valign="center" >
+																					<p>
+																						% Grasa
+																					</p>
+																				</td>
+																	</tr>
+																	<tr style="background:#fff;">
+																				<td align="center" valign="center" >
+																						<p>
+																								66,1
+																						</p>
+																				</td>
+																	</tr>
+													</table>
+													<!--inner table end -->
+
+													<!--inner table start -->
+													<table cellpadding="0" cellspacing="0" border="1" align="center" role="presentation" style="width:97%;margin-bottom: 10px">
+																	<tr style="border-bottom:solid 1px black;background:#FF8AD8;">
+																				<td align="center" valign="center" >
+																					<p>
+																						Kg Grasa
+																					</p>
+																				</td>
+																	</tr>
+																	<tr style="background:#fff;">
+																				<td align="center" valign="center" >
+																						<p>
+																								66,1
+																						</p>
+																				</td>
+																	</tr>
+													</table>
+													<!--inner table end -->
+
+
+
+
+										</td>
+												<td class="block" align="left" valign="center" style="padding:1%;" width="80%">
+																	<img src="http://team-hh.com/wp-content/uploads/2023/06/image-2.webp" width="98%" />
+										</td>
+							</tr>
+			</table>
+			<!--table end -->
+
+
+		</td>
+	</tr>
+	</table>
+</body>
+</html>
